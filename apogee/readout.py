c67c41581d0689b67561f414c344bbdc4cec0ba3
Diplomat <paulmatteschk@googlemail.com>
2024-06-18 18:07:57 +0200

    Update readout.py

--- apogee/readout.py
+++ apogee/readout.py
@@ -11,17 +11,27 @@ i2c = busio.I2C(board.SCL, board.SDA)
 ads = ADS.ADS1115(i2c, address=0x48)
 
 # Set gain
-ads.gain = 16
+ads.gain = 4
 
-# Initialize channel (single-ended on channel 0)
-chan = AnalogIn(ads, ADS.P1)
+# Create single-ended input on channel 0, 1, 2, and 3
+chan0 = AnalogIn(ads, ADS.P0)
+chan1 = AnalogIn(ads, ADS.P1)
+chan2 = AnalogIn(ads, ADS.P2)
+chan3 = AnalogIn(ads, ADS.P3)
+
+# Create differential input between channel 0 and 1
+chan_diff = AnalogIn(ads, ADS.P0, ADS.P1)
 
 print("{:>5}\t{:>5}".format('Raw', 'Volts'))
 
 try:
     while True:
-        print("{:>5}\t{:>5.3f}".format(chan.value, chan.voltage))
-        time.sleep(0.5)
+    print("Channel 0: {:>5}\t{:>5.6f}".format(chan0.value, chan0.voltage))
+    print("Channel 1: {:>5}\t{:>5.6f}".format(chan1.value, chan1.voltage))
+    print("Channel 2: {:>5}\t{:>5.6f}".format(chan2.value, chan2.voltage))
+    print("Channel 3: {:>5}\t{:>5.6f}".format(chan3.value, chan3.voltage))
+    print("Differential (0-1): {:>5}\t{:>5.6f}".format(chan_diff.value, chan_diff.voltage))
+    time.sleep(0.5)
 except KeyboardInterrupt:
     print("Script interrupted by user.")
 except Exception as e:
