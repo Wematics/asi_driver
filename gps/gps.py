56d125ae362a483ba32ba8132f97d36ce9c518f6
Diplomat <paulmatteschk@googlemail.com>
2024-07-08 12:28:25 +0200

    Update gps.py

--- gps/gps.py
+++ gps/gps.py
@@ -1,34 +1,59 @@
 #sudo nano /boot/firmware/config.txt
-#enable_uart=1
+#enable_uart=3
 #sudo reboot
 #sudo apt-get update
 #sudo apt-get install gpsd gpsd-clients python3-gps
-#pip install pyserial
+#mkdir venv
+#cd ~/venv
+#python3 -m venv asi_system
+#source ~/venv/asi_system/bin/activate
+#pip install pyserial adafruit-circuitpython-gps
 
-import serial
+
 import time
+import serial
+import adafruit_gps
 
-# Configure the serial port
-ser = serial.Serial(
-    port='/dev/serial0',  # Use '/dev/serial0' for UART on Raspberry Pi
-    baudrate=9600,  # Baud rate for GPS module
-    timeout=1
-)
+uart = serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=10)
+gps = adafruit_gps.GPS(uart, debug=False)
 
-def read_gps_data():
-    try:
-        while True:
-            if ser.in_waiting > 0:
-                gps_data = ser.readline().decode('utf-8').strip()
-                print(gps_data)
-    except KeyboardInterrupt:
-        print("Exiting Program")
-    except Exception as e:
-        print(f"Error: {e}")
-    finally:
-        ser.close()
+gps.send_command(b'PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
+gps.send_command(b'PMTK220,1000')
 
-if __name__ == "__main__":
-    print("Reading GPS data...")
-    read_gps_data()
+last_print = time.monotonic()
+
+while True:
+    gps.update()
+    current = time.monotonic()
+    if current - last_print >= 1.0:
+        last_print = current
+        if not gps.has_fix:
+            print('Waiting for fix...')
+            continue
+
+        print('=' * 40)
+        print('Fix timestamp: {}/{}/{} {:02}:{:02}:{:02}'.format(
+            gps.timestamp_utc.tm_mon,
+            gps.timestamp_utc.tm_mday,
+            gps.timestamp_utc.tm_year,
+            gps.timestamp_utc.tm_hour,
+            gps.timestamp_utc.tm_min,
+            gps.timestamp_utc.tm_sec))
+        print('Latitude: {0:.6f} degrees'.format(gps.latitude))
+        print('Longitude: {0:.6f} degrees'.format(gps.longitude))
+        print('Fix quality: {}'.format(gps.fix_quality))
+
+        if gps.satellites is not None:
+            print('# satellites: {}'.format(gps.satellites))
+        if gps.altitude_m is not None:
+            print('Altitude: {} meters'.format(gps.altitude_m))
+        if gps.speed_knots is not None:
+            print('Speed: {} knots'.format(gps.speed_knots))
+        if gps.track_angle_deg is not None:
+            print('Track angle: {} degrees'.format(gps.track_angle_deg))
+        if gps.horizontal_dilution is not None:
+            print('Horizontal dilution: {}'.format(gps.horizontal_dilution))
+        if gps.height_geoid is not None:
+            print('Height geo ID: {} meters'.format(gps.height_geoid))
+
 
