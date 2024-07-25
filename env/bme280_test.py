5fd1999942d09e9753a90148c9bec9b6b2e66b47
Diplomat <paulmatteschk@googlemail.com>
2024-07-25 20:04:17 +0200

    Update bme280_test.py

--- env/bme280_test.py
+++ env/bme280_test.py
@@ -1,17 +1,18 @@
 import time
 import board
 from adafruit_bme280 import basic as adafruit_bme280
+
 i2c = board.I2C()  # uses board.SCL and board.SDA
 bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c, address=0x76)
 
 # Read and print the sensor data
 while True:
-    temperature = bme280.get_temperature()
-    humidity = bme280.get_humidity()
-    pressure = bme280.get_pressure()
+    temperature = bme280.temperature
+    humidity = bme280.humidity
+    pressure = bme280.pressure
 
-    print(f"Temperature: {temperature} °C")
-    print(f"Humidity: {humidity} %")
-    print(f"Pressure: {pressure} hPa")
-
+    print(f"Temperature: {temperature:.2f} °C")
+    print(f"Humidity: {humidity:.2f} %")
+    print(f"Pressure: {pressure:.2f} hPa")
+ 
     time.sleep(1)
